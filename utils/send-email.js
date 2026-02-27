ce2f57983612aff83fe5ba912a6809f5dbe48455
savvyaquib <itsmohammadaquib@gmail.com>
2026-02-28 02:19:15 +0530

    use await transporter.sendMail(mailOptions)

--- utils/send-email.js
+++ utils/send-email.js
@@ -3,11 +3,11 @@ import dayjs from 'dayjs'
 import transporter, { accountEmail } from '../config/nodemailer.js'
 
 export const sendReminderEmail = async ({ to, type, subscription }) => {
-  if(!to || !type) throw new Error('Missing required parameters');
+  if (!to || !type) throw new Error('Missing required parameters');
 
   const template = emailTemplates.find((t) => t.label === type);
 
-  if(!template) throw new Error('Invalid email type');
+  if (!template) throw new Error('Invalid email type');
 
   const mailInfo = {
     userName: subscription.user.name,
@@ -28,9 +28,6 @@ export const sendReminderEmail = async ({ to, type, subscription }) => {
     html: message,
   }
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if(error) return console.log(error, 'Error sending email');
-
-    console.log('Email sent: ' + info.response);
-  })
+  await transporter.sendMail(mailOptions);
+  console.log(`Reminder email sent to ${to}`);
 }
